c86ecb784a7caeee50e1a41022096f6ca49cb448
sahil25123 <sahilgupta25123@gmail.com>
2025-12-10 13:09:34 +0530

    feat: configure dotenv and set up server port in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from 'dotenv';
+dotenv.config();
 
+const port=process.env.PORT || 9000;
 
 const app = express();
-
-
 
 app.get("/" ,(req, res)=>{
     res.send("This is the test page ")
 })
 
+app.listen(port ,(req,res)=>{
+    console.log(`Server is Running on Port ${port}`);
+} )
