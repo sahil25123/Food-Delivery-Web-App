473f7171ef617d15fd3f1fc13aa4265307b625bf
sahil25123 <sahilgupta25123@gmail.com>
2025-12-15 01:15:10 +0530

    feat: remove sensitive information logging from database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     const mongo_uri = process.env.MONGO_URI;
-    console.log("DB:", mongo_uri);
+
     try {
         await mongoose.connect(mongo_uri);
         console.log("MongoDB Connected");
