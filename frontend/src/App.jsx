c8468405184bb06f143f0900519362289a873218
sahil25123 <sahilgupta25123@gmail.com>
2025-12-30 14:54:41 +0530

    feat: implement login and registration pages with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/login'
+import Register from './pages/register'
 
 function App() {
 
 
   return (
     <>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <Routes>
+    <Route path={"/login"} element={<Login/>}/>
+    <Route path={"/register"} element={<Register/>}/>
+   </Routes>
    </>
   )
 }
