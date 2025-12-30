73b9f712351572f0d5ae15aadb43855184c23272
sahil25123 <sahilgupta25123@gmail.com>
2025-12-30 18:11:45 +0530

    feat: enhance Register component with styling variables and layout

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Register = () => {
+
+    const primaryColor = "#ff4d2d";
+    const hoverColor= "#e64323";
+    const bgColor="#fff9fd";
+    const borderColor= "#ddd"
+
   return (
-    <div>
+    <div className=' min-h-screen w-full flex items-center justify-center p-4' style={{backgroundColor : bgColor}}>
         Register
       
     </div>
