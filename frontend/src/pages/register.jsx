1dad1beba2f24510e1a66a36d45bea57c3330aa2
sahil25123 <sahilgupta25123@gmail.com>
2025-12-31 11:33:22 +0530

    feat: update Register component layout and styling

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -9,7 +9,10 @@ const Register = () => {
 
   return (
     <div className=' min-h-screen w-full flex items-center justify-center p-4' style={{backgroundColor : bgColor}}>
-        Register
+        <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px] border-[${borderColor}]`}>
+          <h1 className={`text-3xl font-bold mb-2 text`}>Vingo</h1>
+
+        </div>
       
     </div>
   )
